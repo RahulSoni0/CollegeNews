0805501eaf9512f91ad1ca5affda99a74d3cfb03
rahulsoni0 <rahul.kumar.soni2511@gmail.com>
2023-04-10 18:33:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/collegenews/ui/activities/OnboardingActivity.java
@@ -0,0 +1,16 @@
+package com.example.collegenews.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.collegenews.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
